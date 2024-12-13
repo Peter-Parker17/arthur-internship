1d0a2c8bf1ec08e741f0beb8e30257e582828e37
Arthur <menacejuco1163@yahoo.com>
2024-12-13 13:47:45 -0800

    arthur internship3

--- src/components/home/OwlCarousel.jsx
+++ src/components/home/OwlCarousel.jsx
@@ -3,21 +3,51 @@ import OwlCarousel from 'react-owl-carousel';
 import '../home/owl.css'
 
 const MyCarousel = () => {
-  const options = {
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-      0: { items: 1 },
-      600: { items: 6 },
-      1000: { items: 5 }
-    }
-  };
+    const [currentSlide, setCurrentSlide] = useState(0);
 
-  return (
-    <OwlCarousel className="owl" {...options}>
-    </OwlCarousel>
-  );
+    const handleNext = () => {
+        setCurrentSlide(currentSlide + 1);
+    };
+
+    const handlePrev = () => {
+        setCurrentSlide(currentSlide - 1);
+    };
+
+    const options = {
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 6 },
+            1000: { items: 5 }
+        }
+    };
+
+    return (
+        <div>
+
+            <OwlCarousel className="owl" {...options}
+                items={1}
+                loop
+                margin={10}
+                nav={false}
+                dots={false}
+                ref={(owl) => (this.owl = owl)}
+                onChange={(event) => setCurrentSlide(event.item.index)}
+            >
+                <div>Item 1</div>
+                <div>Item 2</div>
+                <div>Item 3</div>
+            </OwlCarousel>
+
+            <button onClick={handlePrev}>Prev</button>
+            <button onClick={handleNext}>Next</button>
+
+        </div>
+
+
+    );
 };
 
 export default MyCarousel;
